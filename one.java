81a40ea62b1850ac07498e6a09fd7c0b02079c1a
swejan <gorintaswejan@gmail.com>
2022-10-25 09:39:03 +0530

    a

---
+++ one.java
@@ -0,0 +1,846 @@
+Ashwini Iiht
+Ashwini Mam
+Mirza
+Nishanth
+Dad
+Sumanth
+Harish
+Aish
+Jakir
+G Pradeep
+Ifthak
+Sujeeth
+Prudhvi
+Gnaneshwar
+Jay
+Dheeraj
+Kavya
+Abhi Iiht
+Pradeep
+Jaswanth
+Balaji Pspk
+Dileep
+Anvesh
+Jaswanth
+Bippin Raj
+Mahendra Iiht
+Anju
+E Nikhil
+Daniya
+John
+Chandu Nbkr
+Vamsi
+Shiva Kummar
+Dheeraj
+Tharun
+Vishnu Gili
+Kavya
+Sharan Banglore
+Prakash
+Manish Iiht
+Om Shiva
+Mom
+Mahesh
+Kunal
+Anjani
+Hayat
+Bhargav PG
+Sai Teja
+PresenJIT
+Manish Iiht
+Bonda
+Archana Iiht
+Narendra
+Sumanth Bangalore
+Kishore
+Dad
+Jyothsnavardhan
+Rebel Tweetz Sharan
+Manu
+Mahi Iiht
+G
+Balaji 2
+Bhavya
+Dir RadhaKrishna
+abhi IIHT
+....ifthak
+Balaji Gurram Pspk
+Mahendra Iiht
+PresenJit Sir
+Krunal S iiht
+Manish Iiht
+Archana IIHT
+Hari Bro Ban
+Manikanta Bro Ban
+Vidya Akka
+Hydra Tirupathi
+Pass Port Bhai
+Poorna
+Poornesh
+Hayat
+Dad
+Harish
+Nithish
+Anjinii
+Nani
+Divakar
+Post Xerox
+Harish Nbkr
+Salar Bike
+Naveen
+Samuel Mama
+Vijay Mama
+Haneesha
+Nani Gnani
+Giligili
+Bįňñý
+ooti Vihanis
+Poornesh
+Jaya Prakash
+Bhanu gundu
+Raja Dad
+Nani
+Ramesh Wood
+Bippin Raj
+Stabiliser Repai
+Kavya Aircel
+Tharun
+Fall C
+Chandu Nbkr
+Steev
+Venky Prabhas
+Shiva Baav
+Sai Kummar
+Raj Kummar Tw
+Fanta Print
+Mounika Akka
+Raju
+Kishore Morla
+Jabba
+Vignan
+Nishanth
+Anjini
+Rajesh Uncle
+Jabba
+NTR Fan
+Mahidhar Sir N. B. R
+Saii
+Mahindra
+Dad
+Prathush 
+su
+Pavan Drdl
+Manetta
+Hayat
+Uday
+Sameer
+Sanjay Ba
+N
+Chandu Nbkr
+Siddu
+Avinash
+Punit
+Mahendra
+Chandu
+Jakir
+Praveen
+Nagi
+Kulavardan Spt
+Yeswanth Sai
+Prabhu Beagle
+Pp
+Asee Mama
+Jaya Mam
+Avinash Nbkr
+Jakir
+Raja Vinni
+John
+Raja Anna
+Sid
+Mouli
+Siva Prakash
+Madhusudana Reddy
+Lokesh Dude
+JAGADEESH PEDDAPALEM
+Prathush
+Praveen
+Siva Bhanu
+N Nitiesh
+Sumanth Blacky
+Sai
+Chinna
+Chaithanya 
+Swejan Idea
+Pradeep Aluka
+madan RE
+Vijay Mama
+Mom
+Kirubhakaran
+Decoration Sreenu
+Shammi
+Plaza Opticals
+Dastagiri
+Vinay Sankranthi
+Abhibav
+Sri NBKR
+Hitesh
+Hari
+PRIYANKA NPET
+Pitai
+Yogeshwaran
+Mahi
+Cnc
+Ashok
+ᶠᵀᴷ꧁☬℘ཞąƙąʂɧ☬꧂ Fall
+Bala
+Napa
+Siddu
+Sumanth
+Tharun B
+Sujeeth Charan
+Vineesh Frn
+Tez Fl
+Kavya Idea
+Bharathi Atta
+Klintu
+Babji Studio
+Himavanth
+Prasanna Atta
+Surendra Lm
+Mahesh
+Tiles
+K R
+MAHESH
+Venkatesh(karodu)
+Muni Uncle Carpentor
+Prashanth
+Chandelier Fitting
+Titla
+1104
+Sahana
+Poorna Darling
+Senu Spet
+Jay
+heshitosh
+Eswar
+Balaji 312
+Sabarish
+Manoj
+K R
+Anjini 2
+Fall Cleeling Hindi
+REINA
+Rajesh
+Nani Idea
+Narendra Nbkr
+Kishore NTR
+Dhananjay Uncle
+Srinu Nbkr
+Surya
+Anju
+Tharun Bhai 
+NISHANTH
+Sai Sandeep
+Pavan Satyam
+Asifa
+Mahesh
+Nithin
+Vijay
+Manatta
+Chinni Pinni
+Bonda Visa
+Charan Car
+Jaswanth
+Jan Karthick
+Ganesh clg
+Dheeraj
+Paint Shekar
+K Pavan
+Fiber Net
+Chandra Macha
+Mom
+Dinesh
+Kummar
+Nagi
+Nineeth
+Sai Kiran
+Prasad 12
+Bippin Raj
+Abhishek
+Joshua
+Rajesh Srinu
+Sai Tej
+Anvesh
+Dany
+Sanjay Bava
+Sai Kiran
+Titla
+Vaishu
+Srinu
+Thulasi
+Swejan AC
+DRIVER Sullurpeta
+Sagar Aig
+Punith
+Lokesh reddy
+Siva sundaram
+Jyo Akka
+Jayalakshmi Atta
+Punith
+David
+Shekar Shetty
+Baby
+Vijay Mama
+Ramya
+Poorna
+Sukeesh
+Viany Raja
+Rajesh Dad
+Jungle man
+Sasi Bro
+Vineela Diana
+Hr Mehar Mam
+Johnjadahjan
+Vishnu
+Mohan
+Madhu
+GORINTA JOHNSON
+Cherry
+Kavya Jio
+Praharsh
+Jassu
+Gowry 
+Balaji
+Isuzu ram
+Venkat Zack
+Bhuvanesh
+Ramesh
+Madan
+Sullurpeta
+Bhagavan
+Tharun
+Rupesh Prabhas
+Reina Andriya
+Krish Salon
+Vijji Pinni
+Nishanth
+Srikanth
+Raj
+Sabari Nbkr
+Prudhvi Charan
+Vamsi Nbkr
+Srinu Insurance
+Vineeth NBKR
+Praneeth
+Dell Nellore
+Tvrr Krishn
+Royal Prabhu Pet
+Punith
+Kasyap Bav
+Haneef
+Chenchamma
+Thiluck
+Arudra
+Cnc
+John
+Mirza
+Nishanth
+Praveen Clg
+Om Shiva
+Bonda🤢
+Subbu theater
+Lokesh reddy
+Surendra
+Summanth
+Prakash
+Peter MS
+Hari Prabhas
+Baava 2
+Shiva Kummar
+Bhanu Puli
+Neelam Narendra
+Vekatesh RE
+Tulsi
+Samuel
+K Attamma
+heshitosh
+Hayat basha
+Munna Bhai
+Sai 2
+Avinash
+Vishnu Action
+Prem Kummar
+Satya NBKR
+Mom
+Venky
+Pradeep Eluka
+Amarbabu Sir
+Dileep
+Dheeraj
+Rama uncle
+Usman Driver
+HOD Sir
+Venky
+Cherry
+Aish
+Sumanth 2
+Poorna Nbkr
+Rama Atta
+Nikhil
+Babai
+Sanjay GPay
+Anjinii
+Ivd Saaho 
+Mohan Sai
+Hayat basha
+Ashok
+Chinna
+Praveen
+Sujith Maama
+Biryani
+Likhith Sai
+Kasyap
+Anil
+Bunny
+Cheery
+M
+Cherry
+Gopi Xerox
+Bhuvan
+Adhi
+Sabber
+Danyy
+Sasi Reddy
+Srikanth Bro
+Kiran Babai
+Mahendra
+Anjini Tada
+Anudeep
+Lalithakka
+Gangadhar Prabhas
+Anvesh
+Sai Tp
+Pradeep
+Pradeep
+V Epiq
+Ravi Mama
+Vamsi TT
+Chinna
+Sai Kummar
+Chinnu P
+Prudhvi Charan
+Asifa
+Biryani
+Nikhil
+Mahendra
+Balaji
+N Nitiesh
+K R
+Joshua
+Anjinii
+Haneesha
+Decoration Sreenu
+Prabhas 20 Raadha
+Baava
+Anjini 2
+Srinu Insurance
+Nishanth
+Avinash Nbkr
+Vineeth NBKR
+Sujeeth Charan
+Anju
+Lokesh reddy
+Shiva Kummar
+Anil
+Uday
+Abhishek
+Sai Kiran
+Madhu
+Reina Andriya
+Babai
+Bhagavan
+Rajesh Dad
+Hayat
+Bhanu Puli
+Punith
+Bhuvan
+Chaithanya
+Aish
+Gangadhar Prabhas
+Vijay Mama
+Punith
+Pavan Satyam
+Punith
+Kasyap Bav
+Venkatesh(karodu)
+Pitty
+Abhibav
+Praveen
+Srinu
+Bįňñý
+Nani Gnani
+Himavanth
+Reshma
+Dileep
+Jan Karthick
+Venky
+Arudra
+Manoj
+Surya
+Tulsi
+Tvrr Krishna
+Jakir
+Raja
+Nishanth
+Chinni Pinni
+Sumanth 2
+Titla
+Kishore Morla
+Adhi
+Prathush
+Chandu
+Chandra Macha
+Om Shiva
+K Attamma
+Amarbabu Sir
+Jaya Mam
+Napa
+Mom
+Jabba
+Poorna Darling
+heshitosh
+Sasi Reddy
+Ashok
+Baby
+Nineeth
+Mahindra
+Poornesh
+Vineela Diana
+Charan Car
+Praveen
+Sameer
+Nithish
+Anjini Tada
+Hayat basha
+Mohan
+Rajesh
+Vamsi Nbkr
+John
+Mom
+Bhuvanesh
+Suneel Babai
+Vamsi TT
+Divakar
+Fiber Net
+Mahesh
+Hitesh
+madan RE
+Jyo Akka
+Anudeep
+Himavanth
+Jabba
+Ramya
+Shiva Kummar
+Praneeth
+Sri NBKR
+Anjinii
+Kiran Babai
+Ravi Mama
+Vishnu
+Lokesh reddy
+Nikhil
+Bonda🤢
+Vineesh Frnd
+Dheeraj
+Ivd Saaho
+Harish Nbkr
+Poorna
+Fanta Print 
+Hari Prabhas
+Nishanth
+Mounika Akka
+N Nitiesh
+Jay
+Vaishu
+Sasi Reddy
+Avinash
+Ashok
+Shekar Shetty
+Siva Prakash
+Surendra
+Pradeep
+Mouli
+Sujith Maama
+Siva Bhanu
+Prem Kummar
+Jassu
+Sai Tej
+Sagar Aig
+Venky Prabhas
+Prem Kummar
+Vishnu Action
+Hayat
+Samuels
+Chandu Nbkr
+Satya NBKR
+Tharun
+Anjini
+Yeswanth Sai
+K R 
+Chinna
+Vijji Pinni
+Dhananjay Uncle
+Bharathi Atta
+M
+Hari
+Mom
+Rajesh Uncle
+Harish Nbkr
+Lokesh Dude
+Swejan Idea
+Sanjay Bava
+Pp
+Vinay Sankranthi
+Likhith Sai
+Mirza
+Venky
+Sai Tp 
+Chinnu Pinni
+Bippin Raj
+Narendra Nbkr 
+David
+Ganesh clg
+Mahesh
+Babji Studio
+Kishore NTR
+Sanjay Ba
+Anu
+Pradee Aluka
+Dheeraj
+Bippin Raj
+Jakir
+Haneef
+Salar Bike
+Jayalakshmi Atta
+Babji Studio
+Titla
+Prathush
+Srinu Nbkr
+Shalem
+Praveen
+Yogeshwaran
+Sukeesh
+Gowry
+Dany
+Subbu theater
+Sai Sandeep
+Rama uncle
+Sai 2
+Punit
+Nagi
+Dell Nellore
+Sai Sandeep
+Kavya Jio
+Bala
+Klintu
+Rama Atta
+Swejan AC
+Shiva Baav
+Sahana
+Tez Fl
+Raja Dad
+Nani Idea
+Viany Raja
+Summanth
+Siddu
+Siddu
+Chenchamma
+Sabarish
+Baava 2
+Jungle man
+Sasi Bro
+Sid
+Prakash
+DADY SEZ
+Vijji Pinni
+Nithish
+Sri NBKR
+Vineeth NBKR
+Anjini
+Om Shiva
+Satya NBKR
+Anjini
+Srinu Nbkr
+Tharun
+Napa
+Sumanth
+Bippin Raj
+Ravi Mama
+Nani Gnani
+Jakir
+Bippin Raj
+Gandhi
+Swejan Idea
+Himavanth
+Pavan Drdl
+Eswar
+Nishanth
+Jakir
+Haneef
+John
+Chandu Nbkr
+Nishanth
+Samsung Helpline
+Divakar
+Ganesh clg
+Nishanth
+Bonda
+K Pavan
+Bhanu
+Mahendra
+Prem Kummar
+Hari
+Sid
+N Nitiesh
+Mahendra
+Praneeth
+Vishnu
+Sumanth 2
+Venky
+Mouli
+Shammi
+Dheerath
+Pavan Satyam
+Nani
+Neelam Narendra
+Hayat basha
+Kavya Jio
+Senu Spet
+Prathush
+Tharun Bhai
+Pp
+Jaswanth
+Praharsh
+Jungle man
+Sujeeth Charan
+Abhishek
+Dad Jio
+Nagi
+Tharun B
+Sai Tej
+Poorna
+Nagi
+Jayalakshmi Atta
+Dad
+Dheeraj
+Mahi
+Sai Kiran
+Sanjay Bava
+Pradeep Aluka
+Thiluck
+Mohan Sai
+Madan
+Vijay Mama
+Praveen
+Ashok
+Punith
+Siva sundaram
+Suneel Babai
+Prashanth
+Bhagavan
+Dad
+Bįňñý
+Mahendra Pinni
+Mahindra
+Sasi Reddy
+Nithish
+Prasanna Atta
+Samuel Mama
+Sujith Maama
+Mom
+Suneel Babai
+Hayat basha
+Bharathi Atta
+Prakash
+Dad
+Rama uncle
+Sai Sandeep
+Giligili
+Cherry
+Swejan Jio
+Eluka
+Dany 2
+Dhananjay Uncle
+Sai 2
+Avinash
+Kasyap
+Summanth
+Lokesh reddy
+Vijay Mama
+Shiva Kummar
+Pradeep
+Samuels
+Mummy
+Ganesh 2
+Klintu
+Kulavardan Spt
+Prathush
+Mounika Akka
+Siddu
+Mom
+Punith
+Bala
+Nikhil
+Dheeraj
+Vijji Pinni
+Siva Prakash
+Sasi Bro
+Jabba
+Bhanu gundu
+Giligili
+Sameer
+Arudra
+Asee Mama
+Chinna
+Mahesh
+Rajesh
+Narendra Nbkr
+Prasad 12
+Chandu
+Dany
+Dheeraj
+MAHESH
+Lokesh reddy
+Dheeraj
+Harish School
+Kavya Aircel
+Anvesh
+Chinna
+Chinni Pinni 
+Likhith Sai
+Anvesh
+Praveen
+Dad Airtel
+Suneel Babai
+Jan Karthick
+Chinnu Pinni
+Dad
+Chandu
+Sai Kummar
+Dheeraj Mam's
+Vishnu Action
+Poorna Nbkr
+Venkat Zack
+Pradeep
+Subbu theater
+KAVYA IDIA
+Rama Atta
+Vijay Mama
+Bondaa
+Mummy Jio
+DADY SEZ
+Kavya Idea 
+Dad Bsnl
+Anjini Tada
+Dad1104 
+ 
